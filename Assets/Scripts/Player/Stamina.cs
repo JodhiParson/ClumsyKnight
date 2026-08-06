1b37dac43fd26aac7640e9718b6e66562cbcf8b2
BryceLie <brycelie02@gmail.com>
2026-08-06 17:13:31 -0400

    Added Fuctioning HP and Stamina as well as Sprint

--- Assets/Scripts/Player/Stamina.cs
+++ Assets/Scripts/Player/Stamina.cs
@@ -1,6 +1,52 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Stamina
+public class Stamina : MonoBehaviour
 {
-    
+    public int maxStamina = 100;
+    public float currentStamina;
+    public Slider staminaBar;
+    public Image fillImage;
+    public Color normalColor = Color.white;
+    public Color exhaustedColor = Color.red;
+
+    public bool IsExhausted { get; private set; }
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = currentStamina;
+    }
+
+    public void Drain(float amount)
+    {
+        currentStamina = Mathf.Clamp(currentStamina - amount, 0, maxStamina);
+        staminaBar.value = currentStamina;
+
+        if (currentStamina <= 0f)
+        {
+            IsExhausted = true;
+            UpdateVisuals();
+        }
+    }
+
+    public void Regen(float amount)
+    {
+        currentStamina = Mathf.Clamp(currentStamina + amount, 0, maxStamina);
+        staminaBar.value = currentStamina;
+
+        if (IsExhausted && currentStamina >= maxStamina * 0.5f)
+        {
+            IsExhausted = false;
+            UpdateVisuals();
+        }
+    }
+        private void UpdateVisuals()
+    {
+        if (fillImage != null)
+        {
+            fillImage.color = IsExhausted ? exhaustedColor : normalColor;
+        }
+    }
 }
